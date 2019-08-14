506dc56e8e95c22f3f728828e74a5702e4e9ad5f
Marcin Morawski <marcin@morawskim.pl>
2019-08-14 18:06:08 +0200

    Skrypt do robienia backupow baz mysql uruchomionych w kontenerze

---
+++ scripts/dump-mysql.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+if ARGV.length < 1
+  puts "Dump to argument is required"
+  exit 1
+end
+dump_dir = ARGV[0]
+
+containers = `docker ps -q -f name=mysql`.split
+containers.each do |containerId|
+    puts "#{containerId} looks like mysql container"
+    containerId =  containerId.strip    
+    inspect = `docker inspect #{containerId}`
+    json = JSON.parse(inspect)
+    
+    env = json[0]['Config']['Env']
+    mysql = env.select { |v| v.start_with? 'MYSQL_ROOT_PASSWORD=' }
+    if mysql.length == 1
+        puts "#{containerId} have MYSQL_ROOT_PASSWORD env"
+        length = mysql[0].length
+        root_password = mysql[0][20..length]
+        time = Time.now
+        databases = `docker exec -e MYSQL_PWD=#{root_password} #{containerId} /usr/bin/mysql -uroot -s  -e 'SHOW DATABASES WHERE \`Database\` NOT REGEXP "(^mysql|_schema$)"' | tail -n +1`.split
+        databases.each do |db_name|
+            puts "#{containerId} dump db #{db_name}"
+            file_name = time.strftime("%Y-%m-%d_%H_%M_%S")
+            output = `docker exec -e MYSQL_PWD=#{root_password} #{containerId} /usr/bin/mysqldump --databases #{db_name} -uroot | gzip > #{dump_dir}/#{db_name}-#{file_name}.sql.gz`
+        end
+    end
+end
