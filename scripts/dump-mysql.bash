59faf8c613ed509c1137ed22ca60c1fd05c62398
Marcin Morawski <marcin@morawskim.pl>
2019-08-27 19:51:31 +0200

    Skrypt bash do robienia lokalnej kopi bazy MySQL

---
+++ scripts/dump-mysql.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail -o noclobber
+
+# directory to put the backup files
+BACKUP_DIR=${BACKUP_DIR:-~/backups}
+
+# MYSQL Parameters
+MYSQL_USERNAME=${MYSQL_USERNAME:-root}
+MYSQL_PWD=${MYSQL_PWD:-''}
+# databases seperated by space
+DATABASES=$@
+
+#YYYY-MM-DD-h_m_s
+TIMESTAMP=$(date '+%F-%H_%M_%S')
+
+for database in $DATABASES; do
+    output_file="$BACKUP_DIR/$database-$TIMESTAMP.sql.gz"
+    echo "Dumping database $database to $output_file using mysql account: ${MYSQL_USERNAME}."
+    MYSQL_PWD=$MYSQL_PWD /usr/bin/mysqldump --single-transaction --skip-lock-tables --routines --databases $database -u$MYSQL_USERNAME | gzip > $output_file
+done
