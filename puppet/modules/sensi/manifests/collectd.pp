4beea565d082917c6de25812025669188b65f7f5
Marcin Morawski <marcin@morawskim.pl>
2019-10-09 15:52:12 +0200

    Szablon pliku konfiguracji dla usługi collectd

--- puppet/modules/sensi/manifests/collectd.pp
+++ puppet/modules/sensi/manifests/collectd.pp
@@ -8,4 +8,14 @@ class sensi::collectd {
     enable  => true,
     require => Package['collectd']
   }
+
+  file {'/etc/collectd/collectd.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('sensi/collectd/collectd.conf.erb'),
+    require => Package['collectd'],
+    notify  => Service['collectd']
+  }
 }
