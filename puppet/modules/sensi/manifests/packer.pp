b167c4ef22a57a9d124c8ffd64721303f3292990
Marcin Morawski <marcin@morawskim.pl>
2020-04-30 14:27:00 +0200

    #13 - Update packer to 1.4.5

--- puppet/modules/sensi/manifests/packer.pp
+++ puppet/modules/sensi/manifests/packer.pp
@@ -1,12 +1,12 @@
 class sensi::packer {
-  $version = '1.4.3'
+  $version = '1.4.5'
 
   archive { "/tmp/packer-${version}.zip":
     ensure        => present,
     extract       => true,
     extract_path  => '/tmp/',
     source        => "https://releases.hashicorp.com/packer/${version}/packer_${version}_linux_amd64.zip",
-    checksum      => 'c89367c7ccb50ca3fa10129bbbe89273fba0fa6a75b44e07692a32f92b1cbf55',
+    checksum      => '30da8dab9c526a6d15b037e2234f6f12cf3accfad77eb2c130738ec1a54cab6d',
     checksum_type => 'sha256',
     creates       => '/usr/bin/packer',
     cleanup       => true,
@@ -14,7 +14,7 @@ class sensi::packer {
     ensure         => present,
     source         => '/tmp/packer',
     checksum       => 'sha256',
-    checksum_value => '57e618e4d3fafb152a44d37f208f4e89c7a36e52ce279916fe04b26599fa60f9',
+    checksum_value => '3f2c53501ae8149bf27bbcefe61580241e826977445855fc736851fec143259d',
     owner          => 'root',
     group          => 'root',
     mode           => '0755'
