65d4c0a3898fe71d5969d89a6606ee871825e5c6
Marcin Morawski <marcin@morawskim.pl>
2020-05-16 12:08:26 +0200

    workstation - set KDE konsole default profile

--- puppet/manifest/marcin.pp
+++ puppet/manifest/marcin.pp
@@ -9,4 +9,14 @@ node default {
     'file',
     lookup(marcin::files),
   )
+
+  $user_home_path = '/home/marcin'
+  ini_setting { "[${user_home_path}] konsole set profile name":
+    ensure            => present,
+    path              => "${user_home_path}/.config/konsolerc",
+    section           => 'Desktop Entry',
+    setting           => 'DefaultProfile',
+    value             => "Shell.profile",
+    key_val_separator => '=',
+  }
 }
