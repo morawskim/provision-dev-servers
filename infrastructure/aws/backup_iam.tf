bd75ce03503d73c65ee957d473b523e780b3bbac
Marcin Morawski <marcin@morawskim.pl>
2022-11-09 18:38:04 +0100

    Terraform - create AWS role and user for backup

---
+++ infrastructure/aws/backup_iam.tf
@@ -0,0 +1,63 @@
+resource "aws_iam_role" "backup" {
+  name = "backup_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${local.account_id}:user/backup"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "s3_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+            "s3:PutObject",
+            "s3:GetObject",
+            "s3:ListBucketMultipartUploads",
+            "s3:AbortMultipartUpload",
+            "s3:DeleteObject",
+            "s3:ListMultipartUploadParts",
+            "s3:ListObjectsV2",
+            "s3:ListObjects",
+            "s3:HeadBucket",
+          ]
+          Effect   = "Allow"
+          Resource = ["${aws_s3_bucket.backup.arn}/*"]
+        },
+        {
+          Action   = [
+            "s3:ListBucket",
+            "s3:HeadBucket",
+          ]
+          Effect   = "Allow"
+          Resource = ["${aws_s3_bucket.backup.arn}"]
+        },
+      ]
+    })
+  }
+}
+
+resource "aws_iam_user" "backup" {
+  name = "backup"
+  path = "/"
+}
+
+#resource "aws_iam_access_key" "backup" {
+#  user = aws_iam_user.backup.name
+#}
+
+output "backup_role" {
+  description = "IAM backup role"
+  value       = aws_iam_role.backup.arn
+}
