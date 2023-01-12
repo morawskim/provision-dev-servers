f46f9ca52b9d8effa2ad3bbfbd5c75b141f2912c
Marcin Morawski <marcin@morawskim.pl>
2023-01-12 15:55:55 +0100

    [dockerhub] Manage ssorder repositories in Docker Hub

---
+++ infrastructure/dockerhub/repository_ssorder.tf
@@ -0,0 +1,34 @@
+resource "dockerhub_repository" "ssorder_ssorder" {
+  name        = "ssorder-ssorder"
+  namespace   = "morawskim"
+  description = "Part of ssorder project."
+  full_description = ""
+}
+
+resource "dockerhub_repository" "ssorder_scheduler" {
+  name        = "ssorder-scheduler"
+  namespace   = "morawskim"
+  description = "Part of ssorder project."
+  full_description = ""
+}
+
+resource "dockerhub_repository" "ssorder_api" {
+  name        = "ssorder-api"
+  namespace   = "morawskim"
+  description = "Part of ssorder project."
+  full_description = ""
+}
+
+resource "dockerhub_repository" "ssorder_api_web" {
+  name        = "ssorder-api-web"
+  namespace   = "morawskim"
+  description = "Part of ssorder project."
+  full_description = ""
+}
+
+resource "dockerhub_repository" "ssorder_docs" {
+  name        = "ssorder-docs"
+  namespace   = "morawskim"
+  description = "Part of ssorder project."
+  full_description = ""
+}
