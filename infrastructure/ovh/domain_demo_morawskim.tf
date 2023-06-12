661d5ac1c297ef1def4f49753b0f59da91ce15e9
Marcin Morawski <marcin@morawskim.pl>
2023-06-12 18:59:19 +0200

    [ovh] Additional DNS records for mail server

--- infrastructure/ovh/domain_demo_morawskim.tf
+++ infrastructure/ovh/domain_demo_morawskim.tf
@@ -21,3 +21,31 @@ resource "ovh_domain_zone_record" "demo_mx" {
   ttl       = "60"
   target    = "78.154.68.48"
 }
+
+resource "ovh_domain_zone_record" "demo_mail_spf" {
+  zone      = "mdemo.ovh"
+  subdomain = "mail"
+  fieldtype = "TXT"
+  target    = "v=spf1 mx ~all"
+}
+
+resource "ovh_domain_zone_record" "demo_mx_spf" {
+  zone      = "mdemo.ovh"
+  subdomain = "mx1.mail"
+  fieldtype = "TXT"
+  target    = "v=spf1 mx ~all"
+}
+
+resource "ovh_domain_zone_record" "demo_mail_dmarc" {
+  zone      = "mdemo.ovh"
+  subdomain = "_dmarc.mail"
+  fieldtype = "TXT"
+  target    = "v=DMARC1; p=quarantine; ruf=mailto:postmaster@mail.mdemo.ovh"
+}
+
+resource "ovh_domain_zone_record" "demo_mail_dkim" {
+  zone      = "mdemo.ovh"
+  subdomain = "default._domainkey.mail"
+  fieldtype = "TXT"
+  target    = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA72ZYzgpYL9Dr7EjY6zRpZOY5ecsxYNPJcMJpsck5GD6h/rn+ug5bMH56qWrxIsabWoOp7i4PRSc7dGz/v/mDTQ0vcmlsUznULmdjtBAMVvSO1qwRpeASyeIEfj5aFbT3YPnNvU/oFzPV/MUf2l83sGFVDtfe0V85b7m+sYatvNFCATM+KeV29iYUB0GUmKHUis0dtfV4YDzU5yeEcc2EESpTlXZ4+3QXSrc12ojggBMvL96L+tQAaxwtoWf2uQZ+/XvN7SyuTHDg+wUfy7Smbi0l1Xcw1VVbe9/01PH7EWFYpkqohqRgmyjH+28tlxu915UrnMMnpBwjU3X8+/kOXQIDAQAB/"
+}
