6f74ef0fa3b0b63d7dbc753e3b99fff82bbe3cf2
Marcin Morawski <marcin@morawskim.pl>
2023-10-03 16:21:35 +0200

    [dockerhub] Add lorem-space-backend repository

--- infrastructure/dockerhub/repository_build_via_packer.tf
+++ infrastructure/dockerhub/repository_build_via_packer.tf
@@ -26,3 +26,20 @@ resource "dockerhub_repository" "jsonplaceholder" {
   JSONPlaceholder is powered by [JSON Server](https://github.com/typicode/json-server).
   EOT
 }
+
+resource "dockerhub_repository" "lorem_space_backend" {
+  name        = "lorem-space-backend"
+  namespace   = "morawskim"
+  description = "lorem.space - placeholder images but useful!"
+  full_description = <<-EOT
+  # Backend service for https://lorem.space/
+
+  [Lorem.space](https://lorem.space/) is API for placeholder images.
+
+  The difference between this image and [official image](https://github.com/manasky/lorem-server) is very little.
+  This image contains one photo, which the backend api can returns.
+  This allows to test integration with self-hosted lorem.space for example in package [faker-images](https://github.com/morawskim/faker-images).
+  In lorem.space project exists open task [to share image collection](https://github.com/manasky/lorem.space/issues/9).
+  When that task will be resolve the shared photos will be added to this container image.
+  EOT
+}
