4e40f57da4170a388ec776308a7b9aa342255f1d
Marcin Morawski <marcin@morawskim.pl>
2023-11-09 16:10:01 +0100

    [ovh] SSHFP records

--- infrastructure/dns/domain_morawskim.tf
+++ infrastructure/dns/domain_morawskim.tf
@@ -28,6 +28,48 @@ resource "ovh_domain_zone_record" "intel" {
   target    = "192.168.50.50"
 }
 
+resource "ovh_domain_zone_record" "intel_sshfp_rsa_sha" {
+  zone      = "morawskim.pl"
+  subdomain = "intel"
+  fieldtype = "SSHFP"
+  ttl       = "3600"
+  target    = "1 1 a70a727f382864473c9a99b33b298e15ecd86e50"
+}
+resource "ovh_domain_zone_record" "intel_sshfp_rsa_sha256" {
+  zone      = "morawskim.pl"
+  subdomain = "intel"
+  fieldtype = "SSHFP"
+  ttl       = "3600"
+  target    = "1 2 992caff4bd348ebad5ed45cd874e9b1a508af09ad74039743cb24d8fca83c22f"
+}
+resource "ovh_domain_zone_record" "intel_sshfp_ecdsa_sha" {
+  zone      = "morawskim.pl"
+  subdomain = "intel"
+  fieldtype = "SSHFP"
+  ttl       = "3600"
+  target    = "3 1 cb9acc50aa3d239c59d03b771bd1c4e2c26e8f03"
+}
+resource "ovh_domain_zone_record" "intel_sshfp_ecdsa_sha256" {
+  zone      = "morawskim.pl"
+  subdomain = "intel"
+  fieldtype = "SSHFP"
+  ttl       = "3600"
+  target    = "3 2 9421ef96d379328a553844c3f21415396010e7f06f77b22d43c07ec9d919c5c0"
+}
+resource "ovh_domain_zone_record" "intel_sshfp_ed25519_sha" {
+  zone      = "morawskim.pl"
+  subdomain = "intel"
+  fieldtype = "SSHFP"
+  ttl       = "3600"
+  target    = "4 1 c945deae7d0307c662b6e3c2657aaff54c587a5c"
+}
+resource "ovh_domain_zone_record" "intel_sshfp_ed25519_sha256" {
+  zone      = "morawskim.pl"
+  subdomain = "intel"
+  fieldtype = "SSHFP"
+  ttl       = "3600"
+  target    = "4 2 877a0e0eeae584672b6029cd3f40af877a4fd718da1447583ffba5594a8ed309"
+}
 resource "ovh_domain_zone_record" "rpm" {
   zone      = "morawskim.pl"
   subdomain = "rpm"
