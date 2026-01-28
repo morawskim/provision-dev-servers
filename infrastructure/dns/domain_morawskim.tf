adff81862da07f04e55e129bdffd8dd55435dd3f
Marcin Morawski <marcin@morawskim.pl>
2026-01-28 21:24:51 +0100

    [ovh] Add DNS record for open webui

--- infrastructure/dns/domain_morawskim.tf
+++ infrastructure/dns/domain_morawskim.tf
@@ -16,6 +16,7 @@ locals {
     memos: "noip.morawskim.pl.",
     rss-bridge: "noip.morawskim.pl.",
     ha: "noip.morawskim.pl.",
+    chat: "noip.morawskim.pl.",
   }
   ovh_ipv4 = "94.23.89.116"
   ovh_ipv6 = "2001:41d0:301:5::100"
