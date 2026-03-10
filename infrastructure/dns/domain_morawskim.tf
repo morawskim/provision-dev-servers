587ca1b3f618802224569511bbecf4d63266a594
Marcin Morawski <marcin@morawskim.pl>
2026-03-10 21:45:41 +0100

    [dns] Update intel.morawskim.pl DNS record

--- infrastructure/dns/domain_morawskim.tf
+++ infrastructure/dns/domain_morawskim.tf
@@ -37,7 +37,7 @@ resource "ovh_domain_zone_record" "intel" {
   subdomain = "intel"
   fieldtype = "A"
   ttl       = "60"
-  target    = "192.168.33.13"
+  target    = "192.168.33.14"
 }
 
 resource "ovh_domain_zone_record" "external" {
