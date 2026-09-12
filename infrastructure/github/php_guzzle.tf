9cf51683d214bf4eec3028f907199f0adf9bf539
Marcin Morawski <marcin@morawskim.pl>
2026-09-12 11:46:22 +0200

    [github] Create guzzle-trace-request-middleware repository

--- infrastructure/github/php_guzzle.tf
+++ infrastructure/github/php_guzzle.tf
@@ -7,7 +7,23 @@ resource "github_repository" "php_guzzle_metrics_middleware" {
   has_discussions = false
   has_projects = false
   has_wiki = false
+  # waiting for https://github.com/integrations/terraform-provider-github/pull/3479
+  #has_pull_requests = false
   topics = ["guzzle", "guzzlehttp", "prometheus-metrics", "guzzle-middleware"]
+}
+
+resource "github_repository" "php_guzzle_trace_middleware" {
+  name        = "guzzle-trace-request-middleware"
+  description = "A Guzzle HTTP client middleware that save request and response."
+  visibility = "public"
+
+  has_issues = false
+  has_discussions = false
+  has_projects = false
+  has_wiki = false
+  # waiting for https://github.com/integrations/terraform-provider-github/pull/3479
+  #has_pull_requests = false
+  topics = ["guzzle", "guzzlehttp", "guzzle-middleware"]
 }
 
 resource "github_repository" "php_guzzle_middlewares" {
